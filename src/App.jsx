f86c85e4a17b88bf9c319f6b30d29c08f6630636
sreey <sreelakshmi12898@gmail.com>
2023-07-02 15:54:46 +0530

    Get movie info using SWAPI api

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
 
-const DUMMY_MOVIES = [
-  {
-    id: 'm1',
-    title: 'Random movie 1',
-    openingText: 'This is the caption for movie 1',
-    releaseDate: '2014-08-21'
-  },
-  {
-    id: 'm2',
-    title: 'Random movie 2',
-    openingText: 'This is the caption for movie 2',
-    releaseDate: '2012-05-25'
-  }
-];
-
-
 function App() {
+
+  //Using state to store Movie info
+  const [movie, setMovie] = useState([]);
+
+  //Function the fetches movie info from SWAPI
+  function getMovies(){
+
+    //fetching from the API
+    fetch("https://swapi.py4e.com/api/films/")
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+
+      //Format the object to filter out unwanted info and to change keys
+      const formattedMovie = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl
+        }
+      })
+      setMovie(formattedMovie)
+    })
+  }
+
+  
   return (
     <Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={getMovies} >Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={DUMMY_MOVIES} />
+        <MovieList movies={movie} />
       </section>
     </Fragment>
   );
